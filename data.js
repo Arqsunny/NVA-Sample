fee3d84c5c4f16e4617fd764882808efdf4a8d68
Luis Fernando Useche <Arqsunny@users.noreply.github.com>
2018-07-25 14:49:49 -0500

    Revert "Upload Files"
    This reverts commit 892bd3731e40cf5b4834547cd701fb36a7b8cdd4.

---
+++ data.js
@@ -0,0 +1,132 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-nva---entry-pano-vr_nva_-_entry",
+      "name": "NVA - Entry Pano VR_NVA_-_Entry",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.6231562043547356,
+        "pitch": 0.08496408823736346,
+        "fov": 1.4661211029866583
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.3179865930841004,
+          "pitch": 0.10860682537025923,
+          "title": "Main Courtyard",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-nva---entry-pano-vr_nva_-_fishing_pond",
+      "name": "NVA - Entry Pano VR_NVA_-_Fishing_Pond",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.4977105494402672,
+        "pitch": 0.17154637299898923,
+        "fov": 1.4661211029866583
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.6649495219916854,
+          "pitch": 0.05868811281154862,
+          "title": "Main Courtyard",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-nva---courtyard-pano-vr",
+      "name": "NVA - Courtyard Pano VR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.616480206919217,
+          "pitch": 0.01663511943039886,
+          "rotation": 0,
+          "target": "1-nva---entry-pano-vr_nva_-_fishing_pond"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4173313439177129,
+          "pitch": 0,
+          "title": "Fishing Pond",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "NVA- Pano - Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
